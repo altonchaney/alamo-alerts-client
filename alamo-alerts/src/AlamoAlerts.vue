78ba558f015850ca8f89bf7fdae3f02274549888
Alton Chaney <alton@insito.me>
2018-03-04 16:45:50 -0600

    - restructure vue component to include toggling controls - add some css animations and tweak existing animations - add some feed 'footer' elements

---
+++ alamo-alerts/src/AlamoAlerts.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="alamoalerts">
+    <section class="hero">
+      <div class="background-image"></div>
+      <div class="content">
+        <div class="logo"></div>
+        <p>Get alerted when movies go on sale at your Alamo Drafthouse location!</p>
+      </div>
+    </section>
+    <section class="main">
+      <router-view/>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'AlamoAlerts',
+    data() {
+      return {
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+@import './css/normalize.css';
+@import './css/main.css';
+</style>
--- alamo-alerts/src/components/Listing.vue
+++ alamo-alerts/src/components/Listing.vue
@@ -1,15 +1,67 @@
 <template>
-  <div class="main-list-container">
-    <p class="header">Recently On Sale</p>
-    <ul class="main-list">
-      <li v-for="listing in listings">
-        <p class="release-date">Since {{ listing.timestamp | moment }}</p>
-        <a v-bind:href="'https://drafthouse.com/austin/theater/'+$route.params.locationslug+''" target="_blank">
-          <h2>{{ listing.title }} &raquo;</h2>
-          <h3 v-if="listing.addltimes">ADDITIONAL TIMES!</h3>
-        </a>
-      </li>
-    </ul>
+  <div class="main-container">
+    <div class="location-control-container"
+         v-bind:class="{ open: locationListOpen }">
+      <div class="control">
+        <p class="header">Select a Location</p>
+        <h1 class="active-option" v-on:click="locationListOpen=true">{{ activeLocation.name }} <span class="icon-expand"></span></h1>
+      </div>
+      <div class="list" v-on:click="locationListOpen=false">
+        <ul>
+          <router-link :to="location.slug"
+                       v-for="location in locations">
+            <li>
+              <h1 v-bind:class="{ active: location.slug === activeLocation.slug }">{{ location.name }}</h1>
+            </li>
+          </router-link>
+        </ul>
+      </div>
+    </div>
+    <div class="alert-setup-container" v-bind:class="{ open: alertSetupOpen }">
+      <div class="background" v-on:click="alertSetupOpen=false"></div>
+      <div class="content" v-on:click="alertSetupOpen=true">
+        <div class="alert">
+          <div class="button-content"><span class="icon"></span> SET UP ALERT</div>
+        </div>
+        <ul class="alert-options">
+          <li class="twitter">
+            <a href="https://twitter.com/AlamoAlerts" target="_blank">
+              <button class="button-content"><span class="icon"></span> TWITTER</button>
+            </a>
+            <p class="description">Follow this location’s Twitter account to get alerts in your Feed. Turn on the bell for extra alertness.</p>
+          </li>
+          <li class="rss">
+            <a href="https://twitrss.me/twitter_user_to_rss/?user=alamoalerts" target="_blank">
+              <button class="button-content"><span class="icon"></span> RSS FEED</button>
+            </a>
+            <p class="description">Subscribe to this location’s RSS feed.</p>
+          </li>
+        </ul>
+      </div>
+      <p class="close-modal" v-on:click="alertSetupOpen=false">&times; CLOSE THIS</p>
+    </div>
+    <div class="main-list-container">
+      <p class="header">Recently On Sale</p>
+      <ul class="main-list">
+        <li class="loader" v-if="listings.length === 0"
+                           v-for="index in 6">
+          <div class="release-date"></div>
+          <div class="title"></div>
+        </li>
+        <li v-for="listing in listings">
+          <p class="release-date">Since {{ listing.timestamp | moment }}</p>
+          <a v-bind:href="'https://drafthouse.com/austin/theater/'+$route.params.locationslug+''" target="_blank">
+            <h2>{{ listing.title }} &raquo;</h2>
+            <h3 v-if="listing.addltimes">ADDITIONAL TIMES!</h3>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="feed-footer">
+      <p class="disclaimer">NOTE: Ticket availability subject to change.</p>
+      <a v-bind:href="'https://drafthouse.com/austin/theater/'+$route.params.locationslug+''" target="_blank">
+        <button class="button-content">SEE ALL LISTINGS</button></a>
+    </div>
   </div>
 </template>
 
@@ -22,16 +74,24 @@
 
     data() {
       return {
+        locations: [
+          { id: '0003', name: 'Village', slug: 'village'},
+          { id: '0006', name: 'South Lamar', slug: 'south-lamar'},
+          { id: '0009', name: 'Mueller', slug: 'mueller'}
+        ],
+        activeLocation: {},
+        locationListOpen: false,
+        alertSetupOpen: false,
         listings: []
       }
     },
 
     created() {
-      this.fetchData()
+      this.viewInit()
     },
 
     watch: {
-      '$route': 'fetchData'
+      '$route': 'viewInit'
     },
 
     filters: {
@@ -41,9 +101,15 @@
     },
 
     methods: {
-      fetchData() {
+      viewInit() {
+        // first set the active location
+        const currentSlug = this.$route.params.locationslug
+        this.activeLocation = this.locations.find(function(obj) { return obj.slug === currentSlug })
+        this.locationListOpen = false
+
+        // then set up the location listings
         this.listings.length = 0
-        axios.get('http://localhost:3131/tweets/'+this.$route.params.locationslug)
+        axios.get('http://localhost:3131/tweets/'+currentSlug)
         .then((resp) => {
           for (let i=0; i < resp.data.length; i++) {
             // remove leading text
