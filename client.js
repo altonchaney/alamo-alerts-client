d4294c6916e6d487aba62c78e29925ad4ff07129
Alton Chaney <alton@insito.me>
2018-03-04 20:32:11 -0600

    - use express's built in static module

--- client.js
+++ client.js
@@ -1,12 +1,11 @@
 const express = require('express')
-const serveStatic = require('serve-static')
 const path = require('path')
 
 // create the express app
 const app = express()
 
 // create middleware to handle the serving the app
-app.use("/", serveStatic ( path.join (__dirname, '/dist') ) )
+app.use("/", express.static(__dirname + '/dist'))
 
 // Create default port to serve the app on
 const port = process.env.PORT || 3130
