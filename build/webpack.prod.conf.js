5cbd46a2d755028979f150a48f727cc6a79f1cca
Alton Chaney <alton@ruckit.me>
2021-04-03 18:16:20 -0500

    Remove webpack analyzer

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -141,9 +141,4 @@ if (config.build.productionGzip) {
   )
 }
 
-if (config.build.bundleAnalyzerReport) {
-  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
-}
-
 module.exports = webpackConfig
